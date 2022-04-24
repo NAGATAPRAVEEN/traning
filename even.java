9dde0b7795f623abaa6de98cee640e29379a10e1
Praveenkumar Nagata <p.nagata@zensar.com>
2022-04-24 10:46:47 +0530

    hello

---
+++ even.java
@@ -0,0 +1,17 @@
+public class even {
+    public static void main(String[] args) {
+        int a=23;
+        int b=11;
+        int result=a+b;
+        if (result%2==0) {
+            System.out.println((a+b)+"result is even number");
+            
+        }
+        else
+        System.out.println(a+"result is odd number");
+            
+        }
+    }
+
+    
+
