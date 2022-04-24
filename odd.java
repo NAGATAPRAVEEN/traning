890858d1d0050f982c15eb9899ddcf79dc7b2b34
Praveenkumar Nagata <p.nagata@zensar.com>
2022-04-24 13:32:45 +0530

    hi

---
+++ odd.java
@@ -0,0 +1,15 @@
+public class odd {
+    public static void main(String[] args) {
+        int a=22;
+        if (a%2==1) {
+            System.out.println(a+"it is an odd number");
+        }
+        else
+        {
+            System.out.println(a+"it is an even number");
+        }
+            
+        }
+        
+    }
+    
