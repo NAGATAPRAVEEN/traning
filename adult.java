c1d4684260f19210d264d30b154a16a1d41e0d9d
Praveenkumar Nagata <p.nagata@zensar.com>
2022-04-24 13:21:03 +0530

    heelo

---
+++ adult.java
@@ -0,0 +1,16 @@
+public class adult {
+    public static void main(String[] args) {
+        int age=18;
+        String name="praveen";
+        if (age>=18) {
+            System.out.println(age+name+"he is an adult");
+        }
+        else
+        {
+            System.out.println(age+"he is not an adult");
+        }
+            
+        }
+    }
+    
+
