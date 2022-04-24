f29d5c1a8ef273ed3f9004a3d2be015e9c0a413e
Praveenkumar Nagata <p.nagata@zensar.com>
2022-04-24 10:51:06 +0530

    hello

---
+++ go.java
@@ -0,0 +1,15 @@
+public class go {
+      public static void main(String[] args) {
+          int a=18;
+          if (a>=16) {
+              System.out.println(a+"he is eligible");
+              
+          }
+          else
+          {
+              System.out.println("he is not eligible");
+          }
+          
+      }  
+    }
+    
