19c9a2260a3cd66ab49e39f3f073437e50406fd7
Praveenkumar Nagata <p.nagata@zensar.com>
2022-04-25 13:55:07 +0530

    hm

---
+++ user.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class user {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println(args+ "enter m value");
+        int age=sc.nextInt();
+        System.out.println(args+"eneter the n value");
+        int m=sc.nextInt();
+        for(int i=m;i<=m;i++)
+        {
+            System.out.println("i");
+
+        }
+    }
+    
+}
