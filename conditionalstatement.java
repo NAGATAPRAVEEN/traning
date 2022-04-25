99a7eeaec8977c50f58a358f7573a64ed1cee139
Praveenkumar Nagata <p.nagata@zensar.com>
2022-04-25 09:24:42 +0530

    hu

---
+++ conditionalstatement.java
@@ -0,0 +1,16 @@
+public class conditionalstatement {
+    public static void main(String[] args) {
+        int value=500;
+        if (value>200 || value<500) {
+            System.err.println(value+"item is expensive");
+        }
+        else
+        {
+            System.out.println(value+"item is not expensive");
+        }
+            
+        }
+            
+        }
+    
+    
